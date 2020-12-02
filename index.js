c05d3c12dc10b5cd1f1a6b486360516085d8b9e8
Rick <dirklo@gmail.com>
2020-12-02 11:54:01 +0100

    commit tree 569580bd8e05a7fc4675f4ee277d2c1373fd08ef

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+    const myLists = document.querySelectorAll('.ranked-list');
+    for (let child = 0; child < myLists.length; child++){
+        let children = myLists[child].children;
+        for (let i = 0, l = children.length; i < l; i++) {
+            children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+        }
+    }
+}
+
+function deepestChild() {
+    let deepest = document.querySelector("#grand-node");
+        while (deepest.childElementCount > 0) {
+            if (deepest.children[0]) {
+                deepest = deepest.children[0]
+            }
+        }
+        return deepest
+}
